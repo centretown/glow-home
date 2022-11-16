b8e6a6e603c4d8ae3f8c77a0284af98f1381fa9a
Dave Marsh <davemarsh77@gmail.com>
2022-11-16 18:35:38 -0500

    remove redundant sources

--- esp32-custom/src/glow/Pixels.h
+++ esp32-custom/src/glow/Pixels.h
@@ -10,11 +10,33 @@ namespace glow
   {
   private:
     AddressableLight *light = nullptr;
+    int32_t length = 0;
+    uint16_t rows = 1;
+    uint16_t columns = 1;
+    bool is_setup = false;
+    bool is_logged = false;
 
   public:
-    inline void setup(AddressableLight *it)
+    inline void setup(AddressableLight *it,
+                      uint16_t row_count = 1)
     {
       light = it;
+      length = light->size();
+      set_rows(row_count);
+    }
+
+    void set_rows(uint16_t row_count)
+    {
+      if (row_count < length)
+      {
+        rows = row_count;
+        columns = length / rows;
+      }
+    }
+
+    uint16_t size()
+    {
+      return length;
     }
 
     inline void Put(uint16_t index, ESPHSVColor value)
@@ -22,21 +44,47 @@ namespace glow
       light->get(index) = value;
     }
 
-    inline void Put(uint16_t index, Color &value)
+    inline void Put(uint16_t index, Color value)
     {
       light->get(index) = value;
     }
 
     inline void Update() {}
 
-    inline uint16_t size()
+    void gradient(Color from, Color to)
     {
-      if (light == nullptr)
-      {
-        return 0;
-      }
+      uint8_t amnt = 255 / length;
+      Color color;
 
-      return static_cast<uint16_t>(light->size());
+      auto map = [&](uint16_t index)
+      {
+        if (rows < 2)
+        {
+          return index;
+        }
+        div_t point = div(index, rows);
+        return (uint16_t)(point.rem * columns + point.quot);
+      };
+
+      for (int32_t i = 0; i < length; i++)
+      {
+        color = from.gradient(to, amnt * i);
+        light->get(map(i)) = color;
+      }
+    }
+
+    void log_setup()
+    {
+      is_logged = true;
+    }
+
+    void log_not_setup()
+    {
+      if (!is_logged)
+      {
+        ESP_LOGD("glow-Pixels", "Not Setup!");
+        is_logged = true;
+      }
     }
   };
 
